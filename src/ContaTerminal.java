f69ddeee29063234a1fbef257d67f7fe30c9c965
Amanda Padovan <amandapadovan1991@gmail.com>
2023-10-17 23:07:13 -0300

    finalizando Conta Banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,36 @@
+  import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita ao usuário que insira o número da agência
+        System.out.print("Programa: Por favor, digite o número da Agência! ");
+        String agencia = scanner.nextLine();
+
+        // Solicita ao usuário que insira o número da conta
+        System.out.print("Programa: Por favor, digite o número da Conta! ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpa a quebra de linha
+
+        // Solicita ao usuário que insira o nome do cliente
+        System.out.print("Programa: Por favor, digite o nome do Cliente! ");
+        String nomeCliente = scanner.nextLine();
+
+        // Solicita ao usuário que insira o saldo
+        System.out.print("Programa: Por favor, digite o saldo! ");
+        double saldo = scanner.nextDouble();
+
+         // Fecha o Scanner
+         scanner.close();
+
+        
+         scanner.close();
+ 
+
+
+        // Exibe a mensagem com os dados da conta
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
+
